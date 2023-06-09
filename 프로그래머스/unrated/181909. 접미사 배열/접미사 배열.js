23d684e38f8adfd0cdca7209d99188c8a9451ef7
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-06-09 15:29:10 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = []
+    
+    for (let i = my_string.length-1; i >= 0 ; i--) {
+        answer = [...answer, my_string.slice(i)]
+    }
+    return answer.sort()
+}
