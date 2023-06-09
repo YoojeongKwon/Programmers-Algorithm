ee63da27074c5ddda550b41f8c2465ddc6534a5c
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-06-09 15:18:22 +0900

    접미사인지 확인하기.js

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,3 +1,5 @@
 function solution(my_string, is_suffix) {
     return my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0
 }
+
+// string.endsWith(searchString, length) -> 문자열의 끝이 특정 문자열로 끝나는지 찾기 위한 method, 문자열의 끝이 주어진 문자열로 끝나면 true, 그렇지 않다면 false
