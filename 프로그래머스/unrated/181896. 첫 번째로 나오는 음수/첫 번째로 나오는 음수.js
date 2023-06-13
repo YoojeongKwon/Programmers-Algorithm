15e331095e87597354193212eaab5446d7edc933
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-06-13 14:56:12 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    for(let i = 0; i < num_list.length; i++) {
+            answer++
+        
+        if(num_list[i] < 0) {
+            return i
+        }
+    }
+    
+    if(answer === num_list.length) {
+            return -1
+        }
+}
