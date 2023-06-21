93c865804ec4bd0c8ba108138a26181676dac227
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-06-21 12:18:21 +0900

    Update 조건에 맞게 수열 변환하기 1.js

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -7,3 +7,6 @@ function solution(arr) {
         } else {return num}
     })
 }
+
+// const solution= arr => arr.map(num => num >= 50 && num % 2 == 0 ? num/2 : num < 50 && num % 2 == 1 ? num*2 : num)
+// 삼항 연산자를 이렇게 써도 됨!
