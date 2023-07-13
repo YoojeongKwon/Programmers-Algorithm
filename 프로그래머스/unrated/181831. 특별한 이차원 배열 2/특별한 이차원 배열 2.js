a924503969d8504857e04d9deddd2035d473be88
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-07-13 18:21:03 +0900

    Update 특별한 이차원 배열 2.js

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -13,3 +13,15 @@ function solution(arr) {
     
     return answer.includes(0) ? 0 : 1
 }
+
+// function solution(arr) {    
+//     for(let i = 0; i < arr.length; i++) {
+//         for(let j = 0; j < arr[0].length; j++) {
+//             if (arr[i][j] !== arr[j][i]) {
+//               return 0;
+//             }
+//         }
+//      }
+    
+//     return 1;
+// }
