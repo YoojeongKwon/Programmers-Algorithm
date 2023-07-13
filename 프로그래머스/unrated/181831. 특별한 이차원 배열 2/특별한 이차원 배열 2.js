187bccbc764704ef5f7b07e2e87be4b0c456150f
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-07-13 18:18:34 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    var answer = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] === arr[j][i]) {
+               answer = [...answer, 1] 
+            } else {
+                answer = [...answer, 0]
+            }
+        }
+     }
+    
+    return answer.includes(0) ? 0 : 1
+}
