f39c46ee7b6f30ef46187818de34c251977a4d7f
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-07-16 11:55:55 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,11 +1,5 @@
 function solution(array) {
-    var answer = 0;
+    let max = Math.max(...array);
     
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] - array[i+1] < 0) {
-            answer = array[i+1]
-        }
-    }
-    
-    return [answer, array.indexOf(answer)]
+    return [max, array.indexOf(max)]
 }
