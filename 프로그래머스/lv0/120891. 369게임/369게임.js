09ef5d2fd327aff289afba44b1889120f0cb93b2
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-07-17 02:29:29 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,10 +1,6 @@
 function solution(order) {
-    var answer = 0;
-    
-    for(let i = 0; i < String(order).length; i++) {
-        if(Number(String(order)[i])%3 === 0 && String(order)[i] !== '0') {
-            answer += 1
-        }
-    }
-    return answer;
+  return order
+    .toString()
+    .split("")
+    .filter((v) => v === "3" || v === "6" || v === "9").length;
 }
