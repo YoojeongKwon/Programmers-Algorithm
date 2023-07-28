6fb016992e91085697b378332023794d3c0cce9c
YoojeongKwon <121225163+YoojeongKwon@users.noreply.github.com>
2023-07-29 01:28:42 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,9 +1,6 @@
 function solution(num_list) {
-    let odd = [];
-    let even = [];
-    
-    for(let i = 0; i < num_list.length; i++) {
-        num_list[i]%2 === 0 ? even.push(num_list[i]) : odd.push(num_list[i])
-    }
-    return [even.length, odd.length];
+    return [
+        num_list.filter((num) => num%2 === 0).length,
+        num_list.filter((num) => num%2 === 1).length
+    ]
 }
