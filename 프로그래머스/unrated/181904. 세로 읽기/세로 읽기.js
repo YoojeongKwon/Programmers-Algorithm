a84dc58d5bbec4ca957a518438868db4ee5fd9bf
권유정 <121225163+YoojeongKwon@users.noreply.github.com>
2023-11-07 22:02:32 +0900

    [level unrated] Title: 세로 읽기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,8 +1,8 @@
 function solution(my_string, m, c) {
     var answer = '';
     
-    for(let i = c; i <= my_string.length; i += m) {
-        answer = answer + my_string[i-1]
+    for(let i = c-1; i < my_string.length; i += m) {
+        answer += my_string[i]
     }
     
     return answer
